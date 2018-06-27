7b82528fdce8a70db04e9edc02d7002e493fc0bd
Linus <linushakansson89@gmail.com>
2018-06-27 12:24:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var request = require('request');
-var mysql = require('mysql');
-var connection = connectToDatabase('myHost', 'myUser', 'myPassword', 'myDatabase');
 
 var app = express();
 
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
 //runGetRequest();
-//runDatabaseQuery();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
@@ -22,29 +19,6 @@ function runGetRequest() {
             console.log(error);
         }
     });
-};
-
-//Executes a SQL query
-function runDatabaseQuery() {
-    connection.query("SELECT * FROM SampleTable", function(error, rows, fields) {
-        if(!error) {
-            console.log(rows);
-        } else {
-            console.log(error);
-        }
-    });
-};
-
-// Returns a connection object to the database.
-function connectToDatabase(host, user, password, database) {
-    var connectionJson = {
-        host: host,
-        user: user,
-        password: password,
-        database: database,
-        timezone: 'utc'
-    };
-    return mysql.createConnection(connectionJson);
 };
 
 module.exports = app;
