14aeff8767e7d0486ec95c1e6154415dd09b9b10
nowlow <nowlow77@gmail.com>
2018-07-03 14:49:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var request = require('request');
 var app = express();
 
 app.use(express.static('public'));
-//runGetRequest();
+runGetRequest();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
     
     //sample URL.
-    var url = "http://ip.jsontest.com/";
+    var url = "http://129.157.179.180:3000/shield/33/45/blue/nowlow";
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
